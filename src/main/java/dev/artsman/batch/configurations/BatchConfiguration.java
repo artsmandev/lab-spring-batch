ab584c1dfc9c9cd754af1390a789d01ae2ef320d
Bruno Andrade <bruno@artsman.dev>
2023-07-21 21:02:55 +0100

    feat: created job chunch based with reader/processor/writer

--- src/main/java/dev/artsman/batch/configurations/BatchConfiguration.java
+++ src/main/java/dev/artsman/batch/configurations/BatchConfiguration.java
@@ -4,17 +4,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@EnableBatchProcessing
-@Configuration
 @Log
 @RequiredArgsConstructor
 public class BatchConfiguration {
