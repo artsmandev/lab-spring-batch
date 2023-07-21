83b3485313cd4d2ca750f66d90e2d3304ffef172
Bruno Andrade <bruno@artsman.dev>
2023-07-21 21:32:07 +0100

    refact: hello world job

---
+++ src/main/java/dev/artsman/batch/configurations/jobs/HelloWorldJobConfiguration.java
@@ -0,0 +1,27 @@
+package dev.artsman.batch.configurations.jobs;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableBatchProcessing
+@Configuration
+@Log
+@RequiredArgsConstructor
+public class HelloWorldJobConfiguration {
+	private final JobBuilderFactory jobFactory;
+
+	@Bean
+	public Job helloWorldJob(Step helloWorldStep) {
+		return this.jobFactory.get("Job: Hello World")
+													.start(helloWorldStep)
+													.incrementer(new RunIdIncrementer())
+													.build();
+	}
+}
